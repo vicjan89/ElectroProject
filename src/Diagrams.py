315d5ad35654d61750e3da10f691b1888f02927b
Yanush Viktar <vicjan89@gmail.com>
2022-06-13 17:37:48 +0300

    Элементы преобразовываются в блоки и выводятся в файл матрицей для последующего перетаскивания в нужные позиции

--- src/Diagrams.py
+++ src/Diagrams.py
@@ -1,30 +1,52 @@
 '''Модуль прорисовки проектной документации.'''
-
+import random
 import ezdxf
 from src.GraphWithConnection import *
 
 class CircuitDiagram:
     '''Вывод принципиальной схемы в формат DXF.'''
 
-    def __init__(self, name_file: str, wires_list: list, elements_list, d):
+    def __init__(self, name_file: str, wires_list: list, d):
         '''Вывод принципиальной схемы в формат DXF.
 
         :param name_file: имя файла
         :param wires_list: список проводников.
+        :param d: словарь с элементами схемы.
         '''
 
+        self.name_file = name_file
+        self.wires_list = wires_list
+        self.dict_el = d
+
+    def place_elements(self):
         doc = ezdxf.new()
         doc.units = ezdxf.units.MM
         msp = doc.modelspace()
+        num = 0
+        blocks_list = []
+        nx = 0
+        ny = 0
+        elements = []
+        for key, value in self.dict_el.items():
+            for key_e, value_e in value.connections.items():
+                if value_e[1] not in elements:
+                    elements.append(value_e[1])
+                    blocks_list.append(doc.blocks.new(name=str(num)))
+                    value_e[1].show(blocks_list[-1])
+                    x = nx * 30
+                    y = ny * 30
+                    msp.add_blockref(str(num),(x,y))
+                    num += 1
+                    if nx == 10:
+                        nx = 0
+                        ny +=1
+                    else:
+                        nx += 1
 
-        for i in elements_list:
-            d[i[0]].connections[i[1]][1].mov_to(i[1], i[2], i[3])
-            d[i[0]].connections[i[1]][1].show(msp)
+        # for w in wires_list:
+        #     w.show(msp)
 
-        for w in wires_list:
-            w.show(msp)
-
-        doc.saveas(name_file + '.dxf', encoding='utf-8')
+        doc.saveas(self.name_file + '.dxf', encoding='utf-8')
 
 
 
